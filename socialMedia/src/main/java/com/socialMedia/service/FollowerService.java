19c3121ded63380f29ddd1079cbde7543d99b29a
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:27:11 +0530

    Formatting and Logger added

--- socialMedia/src/main/java/com/socialMedia/service/FollowerService.java
+++ socialMedia/src/main/java/com/socialMedia/service/FollowerService.java
@@ -3,26 +3,28 @@ package com.socialMedia.service;
 /**
  * Follower service that allows the capability to follow or unfollow a followee.
  * A Follower/followee is expected to be a user.
+ * 
  * @author mayankjalotra
  * @since v1.0
  */
 public interface FollowerService {
-	
+
 	/**
 	 * Follower follows a followee.
+	 * 
 	 * @param followerId
 	 * @param followeeId
 	 * @return
 	 */
 	public boolean follow(Long followerId, Long followeeId);
-	
-	
+
 	/**
 	 * Follower unfollows a followee.
+	 * 
 	 * @param followerId
 	 * @param followeeId
 	 * @return
 	 */
 	public boolean unfollow(Long followerId, Long followeeId);
-	
+
 }
