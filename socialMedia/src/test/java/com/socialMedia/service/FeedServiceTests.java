1b93a506f8ec0b98d9f2ec8790df5da74279252a
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:35:36 +0530

    Update FeedServiceTests.java

--- socialMedia/src/test/java/com/socialMedia/service/FeedServiceTests.java
+++ socialMedia/src/test/java/com/socialMedia/service/FeedServiceTests.java
@@ -22,36 +22,38 @@ import com.socialMedia.entity.Post;
 import com.socialMedia.entity.User;
 import com.socialMedia.repository.PostJpaRepository;
 import com.socialMedia.service.impl.FeedServiceImpl;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FeedServiceTests {
-	
+
 	@Mock
 	PostJpaRepository postJpaRepository;
-	
+
 	@InjectMocks
 	FeedServiceImpl feedService;
-	
+
 	@Before
-    public void setUp(){
-        MockitoAnnotations.initMocks(this);
-    }
-	
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void getNewsFeed_test() {
-		List<Post> posts = Arrays.asList(new Post(324L, "con", new User(), new Date()), new Post(323L, "nifeb", new User(), new Date()));
+		List<Post> posts = Arrays.asList(new Post(324L, "con", new User(), new Date()),
+				new Post(323L, "nifeb", new User(), new Date()));
 		when(postJpaRepository.findTop20PostForFeeds(10011L)).thenReturn(posts);
-        
-        String postString = feedService.getNewsFeed(10011L);
-        
-        assertEquals("return type string check", postString, posts.toString());
-        
-        boolean expectedSizeLessThan20 = true;
-        //since number of comma's will be 19 doing a less than check
-        boolean actualFeedSize = postString!=null?postString.split(",").length<20:false;
-        assertSame("feed size less than equal to 20", expectedSizeLessThan20, actualFeedSize);
-        
-        verify(postJpaRepository).findTop20PostForFeeds(10011L);
+
+		String postString = feedService.getNewsFeed(10011L);
+
+		assertEquals("return type string check", postString, posts.toString());
+
+		boolean expectedSizeLessThan20 = true;
+		// since number of comma's will be 19 doing a less than check
+		boolean actualFeedSize = postString != null ? postString.split(",").length < 20 : false;
+		assertSame("feed size less than equal to 20", expectedSizeLessThan20, actualFeedSize);
+
+		verify(postJpaRepository).findTop20PostForFeeds(10011L);
 	}
-	
+
 }
