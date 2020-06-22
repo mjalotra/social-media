6f55df672698ba698cf97db7a69347530d7f7bec
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:34:48 +0530

    Update SocialMediaApplicationTests.java

--- socialMedia/src/test/java/com/socialMedia/SocialMediaApplicationTests.java
+++ socialMedia/src/test/java/com/socialMedia/SocialMediaApplicationTests.java
@@ -2,7 +2,6 @@ package com.socialMedia;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-
 import com.socialMedia.service.FeedServiceTests;
 import com.socialMedia.service.FollowerServiceTests;
 import com.socialMedia.service.PostServiceTests;
@@ -10,6 +9,5 @@ import com.socialMedia.service.PostServiceTests;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({PostServiceTests.class, FeedServiceTests.class, FollowerServiceTests.class})
 class SocialMediaApplicationTests {
-
-
+  
 }
