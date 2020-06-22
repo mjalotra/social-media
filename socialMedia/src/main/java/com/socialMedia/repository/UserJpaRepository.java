e0ea8df612b71622384ec3f617664f1d59960c18
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:30:44 +0530

    Logger and Formatting added

--- socialMedia/src/main/java/com/socialMedia/repository/UserJpaRepository.java
+++ socialMedia/src/main/java/com/socialMedia/repository/UserJpaRepository.java
@@ -4,17 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.socialMedia.entity.User;
+
 /**
- * User repository to fetch data related to user info, its followees and other relations.
+ * User repository to fetch data related to user info, its followees and other
+ * relations.
  * 
  * @author mayankjalotra
  * @since v1.0
  */
 @Repository
-public interface UserJpaRepository  extends JpaRepository<User, Long>{
-	
-//	@Query(value = "select uf.followee_id FROM user_followee uf where uf.user_id = :userId", nativeQuery= true)
-//	public Set<Long> findFolloweesByUserId(@Param("userId") Long userId);
-	
+public interface UserJpaRepository extends JpaRepository<User, Long> {
 
 }
