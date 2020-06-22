e81139971e383e419fb70522b2773d2c3ee555f0
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:21:54 +0530

    Formatting and Logger added

--- socialMedia/src/main/java/com/socialMedia/service/impl/FeedServiceImpl.java
+++ socialMedia/src/main/java/com/socialMedia/service/impl/FeedServiceImpl.java
@@ -1,19 +1,25 @@
 package com.socialMedia.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.socialMedia.repository.PostJpaRepository;
 import com.socialMedia.service.FeedService;
+
 /**
- * Feed service impl to generate the user feed based upon their interest of followees.
+ * Feed service impl to generate the user feed based upon their interest of
+ * followees.
  *
  * @author mayankjalotra
  * @since v1.0
  */
 @Service
 public class FeedServiceImpl implements FeedService {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(FeedServiceImpl.class);
+
 	@Autowired
 	private PostJpaRepository postJpaRepository;
 
@@ -22,8 +28,8 @@ public class FeedServiceImpl implements FeedService {
 	 */
 	@Override
 	public String getNewsFeed(Long userId) {
+		logger.debug("Fetching top 20 News Feed");
 		return postJpaRepository.findTop20PostForFeeds(userId).toString();
 	}
-
 
 }
