4b9f88d8bbe08c833c6d6367f22284080d69a6de
mjalotra <50691295+mjalotra@users.noreply.github.com>
2020-06-22 19:29:48 +0530

    Formatting and Logger added

--- socialMedia/src/main/java/com/socialMedia/repository/PostJpaRepository.java
+++ socialMedia/src/main/java/com/socialMedia/repository/PostJpaRepository.java
@@ -8,27 +8,26 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.socialMedia.entity.Post;
+
 /**
  * User's Post repository to manage their posts related operations.
+ * 
  * @author mayankjalotra
  * @since v1.o
  */
 @Repository
 public interface PostJpaRepository extends JpaRepository<Post, Long> {
-	
-//	@Query(value="SELECT * FROM POST where posted_by in (:userFollowingSet) order by create_time desc limit 20", nativeQuery=true)
-//	public List<Post> findTop20PostByUserIdIn(@Param("userFollowingSet") Set<Long> userFollowingSet);
-	
+
 	/**
-	 * Method that feches a list of top 20 post or feeds to be
-	 * visible on user's page based upon their interest of followees.
+	 * Method that feches a list of top 20 post or feeds to be visible on user's
+	 * page based upon their interest of followees.
+	 * 
 	 * @param userId
 	 * @return List<Post>
 	 */
-	@Query(value="select p.*, u.name from post p "
+	@Query(value = "select p.*, u.name from post p "
 			+ "inner join user_followee uf on (uf.followee_id=p.posted_by) or (uf.user_id=p.posted_by)"
-			+ " inner join user u on p.posted_by = u.id "
-			+ "where uf.user_id= :userId "
-			+ "order by create_time desc limit 20", nativeQuery=true)
+			+ " inner join user u on p.posted_by = u.id " + "where uf.user_id= :userId "
+			+ "order by create_time desc limit 20", nativeQuery = true)
 	public List<Post> findTop20PostForFeeds(@Param("userId") Long userId);
 }
